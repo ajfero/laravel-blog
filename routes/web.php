40190081a0ab81e9beb61a3722b7c98a171f0e61
ajfero <ajfero.dev@gmail.com>
2022-12-13 15:31:51 -0300

    build; create routes and return views or functions.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// http://laravel9.test/public/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// return a view
+Route::view('/', 'welcome');        // http://laravel9.test/
+Route::view('/contact', 'contact'); // http://laravel9.test/contact
+Route::view('/blog', 'blog');       // http://laravel9.test/blog
+Route::view('/about', 'about');     // http://laravel9.test/about
+
+// return a function
+Route::match(['put' , 'patch' ], '/selectMethods' , function () {
+    //
+});
+
+Route::any( '/allMethods' , function () {
+    //
 });
