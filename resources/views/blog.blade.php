6b9f7b70e81ff7fb8d4461d44c392e2e5f054a2d
ajfero <ajfero.dev@gmail.com>
2022-12-16 10:53:59 -0300

    build: a resource for a route (data in route and show in a view)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,11 @@
 <x-layouts.app >
     <x-slot name="title" > Blog </x-slot> 
     <h1>Blog</h1>
+    <!-- Para inspeccionar los datos que vienen desde la ruta podemos usar dump -->
+    <!-- @dump($posts) -->
+    @foreach ( $posts as $post )
+        <h3>
+            {{ $post['title'] }}
+        </h3>
+    @endforeach
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
+| variables
+|--------------------------------------------------------------------------
+|
+*/
+
+$posts = [
+    [ 'title' => 'First post' ],
+    [ 'title' => 'Second post' ],
+    [ 'title' => 'Third post' ],
+    [ 'title' => 'Fourth post' ],
+];
+
+/*
+|--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -25,8 +39,10 @@ Route::get('/main', function () {
 
 // return a view
 Route::view('/', 'welcome')->name('welcome');          // http://laravel9.test/
+
+// las rutas pueden recibir como tercer valor datos.
+Route::view('/blog', 'blog', ['posts'=>$posts])->name('blog');         // http://laravel9.test/blog
 Route::view('/contact', 'contact')->name('contact') ;  // http://laravel9.test/contact
-Route::view('/blog', 'blog')->name('blog');         // http://laravel9.test/blog
 Route::view('/about', 'about')->name('about');       // http://laravel9.test/about
 Route::view('/security', 'security')->name('security');       // http://laravel9.test/about
 
@@ -38,4 +54,3 @@ Route::match(['put' , 'patch' ], '/selectMethods' , function () {
 Route::any( '/allMethods' , function () {
     //
 });
-
