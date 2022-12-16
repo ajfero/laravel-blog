932f5b9e69899a01a6818c8c1047ef06c0cd1639
ajfero <ajfero.dev@gmail.com>
2022-12-15 21:21:28 -0300

    build: apply components and layouts with attribute with names and properties.

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,9 +1,29 @@
-@extends('layouts.app')
+<!-- 
+    Para modificar los atributos de un template creamos nuestros atributos con 
+    kebac-case y nuestra
+    Todo lo que este dentro x-layout llegara a layout como slot
 
-@section('title','Blog')
-
-@section('content')
-
-    <h1> Blog </h1>
+    attribute of slot specific 
+-->
+<x-layout
+    name="description"
+    meta-description="Blog meta description" 
     
-@endsection
+>
+
+    <!-- 
+        Para imprimir estructuras HTML
+        Podemos agregar slot con su nombre
+        los slot llegaran a layout como propiedades según el nombre
+        for load logic we can put colon before the attribute
+    -->
+    <x-slot
+        name="title" 
+        :suma="2 + 2"
+        > Blog 
+    </x-slot> 
+
+    <!-- slot-layout content -->
+    <h1>Blog</h1>
+
+</x-layout>
---
+++ resources/views/cart.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app
+    name="description"
+    meta-description="Blog meta description" >
+    <x-navigation/>
+    
+    <x-slot name="title" > Cart </x-slot> 
+    <h1>Cart</h1>
+
+</x-layouts.app>
