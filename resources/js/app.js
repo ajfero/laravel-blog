3b20d34885035699c18dfdbbceddd327d9809c3e
Anthony F <ajfero.dev@gmail.com>
2023-01-10 10:58:48 -0300

    build: implement SASS , VITE and JS into the Project. Install dependency sass, run de command npm run build for production

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 import './bootstrap';
+
+console.log('Js Loaded');
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $metaDescription ?? 'Default meta-description' }}">
     <title> App - {{ $title ?? 'default' }} </title>
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.scss','resources/js/app.js'])
 </head>
+
 <body>
-    <x-layouts.navigation/>
-    
+    <x-layouts.navigation />
+
     <!-- Estos mensajes modificaran vuestro html y podra romper la maqueta.
     por lo que se presentaran solo cuando esten disponibles con esta condicional -->
     @if (session('status'))
-        <div> {{ session('status') }} </div>
+    <div> {{ session('status') }} </div>
     @endif
 
     {{$slot}}
 </body>
+
 </html>
