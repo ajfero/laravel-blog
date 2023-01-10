e05e2749f6de91d7562ec50fd246ec959ca169e5
Anthony F <ajfero.dev@gmail.com>
2023-01-10 09:15:22 -0300

    build: massive-assign ok but we can't use the method all() into the controller for security.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,8 +12,24 @@ class Post extends Model
     // change de name of Model for the name of the table
     // protected $table = 'articules';
 
-    // add  to fillable property to allow mass assignment
-    // asignacion masiva.
-    protected $fillable = ['title','body'];
+    // add  to fillable property to allow massice assignment
+    // asignacion masiva. Si le pasamos todos los datos podemos hacer la assignacion masiva. 
+    // protected $fillable = ['title','body'];
 
+    // podemos definir lo opuesto de los campos que no podemos ingresar a la base de datos.
+    // Si lo dejamos sin alguno estamos informando que podemos dejar pasar a la DB todos los campos del form.
+    protected $guarded = [];
+    // este nos dejara pasar todo el form con toda la data incluyendo el token CSRF. Esto es un problema de seguridad grave.
+    // con lo que podemos evitar enviarlo es evitar usando el methodo all() dentro del controlador correspondiente.
+    // teniendo en cuenta que las validaciones ocurren en el request.
+
+    /* tambien podemos deshabilitar la asignacion masiva en el
+        AppSertviceProvider.php
+            use Illuminate\Database\Eloquent\Model;
+
+            public function boot()
+            {
+                Model::unguard();
+            }
+    */
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Desahbilita la protecion contra la asignacion masiva
+        //  Model::unguard();
     }
 }
